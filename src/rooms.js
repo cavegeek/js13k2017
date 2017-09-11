04f21afc3865c14f88e1f298a69a7b81b110f523
Jason Stumpf <jason.stumpf@gmail.com>
2017-09-10 17:39:42 -0700

    Big fixes, better tick function is broken.

--- src/rooms.js
+++ src/rooms.js
@@ -1,27 +1,35 @@
-function hydroponic() {
-  var prod = Math.min(500, Math.max(ship.co2, 0));
-  ship.food += prod/250/ticks_per_day; // Litres of CO2 to kilograms
-  ship.oxygen += prod/ticks_per_day;
-  ship.co2 -= prod/ticks_per_day;
-}
-
-function water() {
-  if(ship.waste_water > 20/ticks_per_day) {
-    ship.waste_water -= 20/ticks_per_day;
-    ship.clean_water += 20/ticks_per_day;
+var hydroponic = {
+  name: "hyroponic",
+  up: {
+    oxygen: 500,
+    food: 2
+  },
+  down: {
+    co2: 500,
+    clean_water: 2
   }
-}
-
-function oxygen() {
-  if(ship.oxygen < rooms.length*50000*23.5/100 || ship.co2 > rooms.length*50000*0.04/100) {
-    ship.oxygen += 3000/ticks_per_day;
-    ship.co2 -= 3000/ticks_per_day;
+};
+var water = {
+  name: "water",
+  up: {
+    clean_water: 20
+  },
+  down: {
+    waste_water: 20
   }
+};
+var oxygen = {
+  name: "oxygen",
+  up: {
+    oxygen: 3000
+  },
+  down: {
+    co2: 3000
+  }
+};
+var empty = {
+  name: "empty",
+  up: {},
+  down: {}
 }
-
-function empty() {
-}
-
 var room_types = [hydroponic, water, oxygen, empty];
-
-
